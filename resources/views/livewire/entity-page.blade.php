1c8f5c44a0557e1d77e73983f1b4626e2e8a6cba
unknown <melv_purz10@yahoo.com>
2024-04-30 19:39:39 +0800

    modified:   resources/views/livewire/entity-page.blade.php

--- resources/views/livewire/entity-page.blade.php
+++ resources/views/livewire/entity-page.blade.php
@@ -7,65 +7,66 @@
             <div class="grid place-items-center">
                 
             @if(!is_null($entity->image))
-                <div id="default-carousel" class="relative w-full" data-carousel="slide">
-                    <!-- Carousel wrapper -->
-                    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                @if(count($entity->image) > 1)
+                <div id="entityCarousel" class="relative w-full flex-auto"  data-te-carousel-init data-te-ride="carousel">
+                <!--Carousel indicators-->
+                    <div class="absolute top-5 left-0 right-0 z-40 mx-[15%] mb-4 flex list-none justify-center p-0" data-te-carousel-indicators>
+                        @foreach($entity->image as $img)
+                            @if($loop->iteration == 1)
+                                <button
+                                type="button"
+                                data-te-target="#entityCarousel"
+                                data-te-slide-to="{{$loop->iteration - 1}}"
+                                data-te-carousel-active
+                                class="mx-[3px] box-content h-[10px] w-[10px] md:h-[15px] md:w-[15px] rounded-full flex-initial cursor-pointer border-solid border-transparent bg-yellow-600 p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+                                aria-current="true"
+                                aria-label="Slide {{$loop->iteration}}"></button>
+                            @else
+                                <button
+                                type="button"
+                                data-te-target="#entityCarousel"
+                                data-te-slide-to="{{$loop->iteration - 1}}"
+                                class="mx-[3px] box-content h-[10px] w-[10px] md:h-[15px] md:w-[15px] rounded-full flex-initial cursor-pointer border-solid border-transparent bg-yellow-600 p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+                                aria-label="Slide {{$loop->iteration}}"></button>
+                            @endif
+                        @endforeach
+                    </div>
+                    <!-- Carousel items -->
+                    <div class="relative flex h-full w-full overflow-hidden after:clear-both after:block after:content-['']">
+                        
+                        @foreach($entity->image as $img)
+                            @if($loop->iteration == 1)
+                            <div
+                            class="relative float-left -mr-[100%] w-full max-w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none object-fill"
+                            data-te-carousel-item
+                            data-te-carousel-active>
+                            @else
+                            <div
+                            class="relative float-left -mr-[100%] hidden w-full max-w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+                            data-te-carousel-item>
+                            @endif
+
+                            <img
+                                src="{{ asset('/storage/'.$img) }}"
+                                class="block max-w-full m-auto"
+                                alt="{{$entity->title}} Image" />
+                            </div>
+                        @endforeach
+
                         <!-- Item 1 -->
-                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <!-- <div class="hidden duration-700 ease-in-out" data-carousel-item>
                             <img src="/docs/images/carousel/carousel-1.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                        </div>
-                        <!-- Item 2 -->
-                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="/docs/images/carousel/carousel-2.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                        </div>
-                        <!-- Item 3 -->
-                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="/docs/images/carousel/carousel-3.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                        </div>
-                        <!-- Item 4 -->
-                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="/docs/images/carousel/carousel-4.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                        </div>
-                        <!-- Item 5 -->
-                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="/docs/images/carousel/carousel-5.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                        </div>
+                        </div> -->
                     </div>
-                    <!-- Slider indicators -->
-                    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                    </div>
-                    <!-- Slider controls -->
-                    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                            </svg>
-                            <span class="sr-only">Previous</span>
-                        </span>
-                    </button>
-                    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                            </svg>
-                            <span class="sr-only">Next</span>
-                        </span>
-                    </button>
                 </div>
-                @foreach($entity->image as $img)
+                @else
                 <div class="">
-                    <img src="{{ asset('/storage/'.$img) }}" alt="{{$entity->title}} Image" class="min-w-10 min-h-10 h-auto max-w-full rounded-2xl">
+                    <img src="{{ asset('/storage/'.$entity->image[0]) }}" alt="{{$entity->title}} Image" class="min-w-10 min-h-10 h-auto max-w-full rounded-2xl">
                 </div>
-                @endforeach
-                </div>
+                @endif
             @else
                 <div class="text-lg">
-                    <img src="{{ asset('img/ccp-default-big.png') }}" alt="Featured Image" class="min-w-10 min-h-10 max-h-[300px] rounded-2xl">
+                    <img src="{{ asset('img/ccp-default-big.png') }}" alt="No Image" class="min-w-10 min-h-10 max-h-[300px] rounded-2xl">
                     <!-- No Image Found -->
                 </div>
                 <p id="no-images">No images in folder</p>
