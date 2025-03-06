95ca60b5a3a9875fe875aa1b2feab55824afb5ce
unknown <melv_purz10@yahoo.com>
2025-03-06 15:21:55 +0800

    new file:   database/migrations/2025_03_06_033346_update_entities_columns.php

---
+++ database/migrations/2025_03_06_033346_update_entities_columns.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            //
+            $table->boolean('inventory')->nullable()->change();
+            $table->boolean('thumbnail')->nullable()->change();
+            $table->text('subparent_collection')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            //
+        });
+    }
+};
